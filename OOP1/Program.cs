28c43fa2c693784db5abe7503c201ef9d56afc10
Kübra Terzi <kubraterzi0303@gmail.com>
2021-01-22 04:44:50 +0300

    Added Text

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,7 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Loading Codes");
         }
     }
 }
