d645f1e14f6740e8bb8bf7943b81aec77ea7f1c7
Kübra Terzi <kubraterzi0303@gmail.com>
2021-01-23 17:32:48 +0300

    Added - GenericTypesDİctionary

---
+++ DictionaryList2/DictionaryList2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryList2
+{
+    public class DictionaryList2<TKey, TValue>
+    {
+        Dictionary<TKey, TValue> openWith = new Dictionary<TKey, TValue>();
+        public void Add(TKey key, TValue value)
+        {
+            openWith.Add(key, value);
+        }
+        public void GetAll()
+        {
+            foreach (var item in openWith)
+            {
+                Console.WriteLine(" Key :  {0} |    Values : {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
---
+++ DictionaryList2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DictionaryList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryList2<string, int> List = new DictionaryList2<string, int>();
+
+            List.Add("Kübra", 26);
+            List.Add("Ahmet", 13);
+            List.Add("Mehmet", 20);
+
+            List.GetAll();
+
+        }
+    }
+}
