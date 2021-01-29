deffe5954ef95204e0b25b76052fd85d7091eff0
Kübra Terzi <kubraterzi0303@gmail.com>
2021-01-29 03:38:06 +0300

    Added- SQLQueries TotalGainGroupByProducts

---
+++ SQLQueries/TotalGainGroupByProducts.sql
@@ -0,0 +1,5 @@
+﻿
+select ProductName as ProductName, SUM([Order Details].UnitPrice * [Order Details].Quantity*(1-[Order Details].Discount))
+as TotalGain from ((Products inner join [Order Details] on Products.ProductID = [Order Details].ProductID)
+inner join Orders on [Order Details].OrderID= Orders.OrderID) group by Products.ProductName 
+
